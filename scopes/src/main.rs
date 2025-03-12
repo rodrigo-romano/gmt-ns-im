bd9191ec11a8d8c09729ee2344b4ad8bf4a88b73
Rod Conan <rconan@gmto.org>
2025-03-11 17:08:46 -0700

    CREATE SCOPES CLIENT

---
+++ scopes/src/main.rs
@@ -0,0 +1,45 @@
+#![allow(unreachable_code)]
+
+use gmt_dos_clients_io::{
+    gmt_m2::fsm::M2FSMFsmCommand,
+    optics::{SegmentPiston, SegmentWfeRms, WfeRms},
+};
+use gmt_dos_clients_scope_client::Scope;
+use std::env;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    unsafe {
+        env::set_var("SCOPE_SERVER_IP", "44.235.124.92");
+    }
+    loop {
+        if let Ok(scope) = env::var("SCOPE") {
+            match scope.as_str() {
+                "FSM" => Scope::new()
+                    .name("Segment Piston from M1 RBM")
+                    .signal::<M2FSMFsmCommand>()?
+                    .show(),
+                // "M2" => Scope::new()
+                //     .name("Segment Piston from M2 RBM")
+                //     .signal::<M2SegmentPiston>()?
+                //     .show(),
+                // "M2RB" => Scope::new()
+                //     .name("Segment Piston from M2 Reference Body RBM")
+                //     .signal::<M2RBSegmentPiston>()?
+                //     .show(),
+                // "M2S" => Scope::new()
+                //     .name("Segment Piston from M2 Shell Voice Coils")
+                //     .signal::<M2SegmentMeanActuator>()?
+                //     .show(),
+                other => panic!("expected M1, M2, M2RB or M2S, found {other}"),
+            }
+        } else {
+            Scope::new()
+                .name("WFE RMS & Segment Piston")
+                .signal::<SegmentWfeRms<-9>>()?
+                .signal::<WfeRms<-9>>()?
+                .show();
+        };
+    }
+    Ok(())
+}
