27c841075c02613bfd9daafe2cca04f45148fcce
Rod Conan <rconan@gmto.org>
2025-05-02 13:08:44 -0700

    scopes add tracing

--- scopes/build.rs
+++ scopes/build.rs
@@ -1,3 +1,5 @@
+const SCOPE_SERVER_IP: &str = "52.26.134.63";
 fn main() {
-    println!("cargo::rustc-env=SCOPE_SERVER_IP=44.235.124.92");
+    println!("cargo::warning=SCOPE_SERVER_IP={SCOPE_SERVER_IP}");
+    println!("cargo::rustc-env=SCOPE_SERVER_IP={SCOPE_SERVER_IP}");
 }
