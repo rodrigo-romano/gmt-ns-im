238ddf46e676a59c581d7e0eca7f0a90495425e2
Rod Conan <rconan@gmto.org>
2026-02-23 19:10:44 +0000

    qp: added clap cli

--- qp/src/bin/main.rs
+++ qp/src/bin/main.rs
@@ -1,3 +1,4 @@
+use clap::{Args, Parser};
 use gmt_dos_actors::actorscript;
 use gmt_dos_clients::{gif::Gif, operator::Operator, print::Print, timer::Timer};
 use gmt_dos_clients_crseo::{
@@ -6,14 +7,11 @@ use gmt_dos_clients_crseo::{
     crseo::{FromBuilder, Gmt},
     sensors::{Camera, NoSensor},
 };
-use gmt_dos_clients_io::{
-    Estimate,
-    optics::{SensorData, Wavefront, WfeRms},
-};
+use gmt_dos_clients_io::optics::{SensorData, Wavefront, WfeRms};
 use gmt_dos_systems_agws::{
     builder::shack_hartmann::ShackHartmannBuilder,
     kernels::{Kernel, KernelFrame},
-    qp::{ActiveOptics, Estimate2OpticsState, QP},
+    qp::{ActiveOptics, QP},
 };
 use interface::{
     Left, Right, Tick,
@@ -29,9 +27,48 @@ const M1_BM: usize = 27;
 const M1_RBM: usize = 41;
 const M2_RBM: usize = 41;
 
+#[derive(Parser)]
+struct Cli {
+    /// GMT Mirror ID (1 or 2)
+    #[arg(short, long)]
+    mirror: usize,
+    /// GMT segment ID [1,2,...,7]
+    #[arg(short, long)]
+    segment: usize,
+    /// Simulation sample #
+    #[arg(short, long, default_value_t = 20)]
+    n_sample: usize,
+    #[command(flatten)]
+    rbm: Option<Rbm>,
+    #[command(flatten)]
+    mode: Option<Mode>,
+}
+
+#[derive(Args, Clone, Default)]
+// #[group(required = false, multiple = true)]
+struct Rbm {
+    /// GMT RBM index of [Tx,Ty,Tx,Rx,Ry,Rx]
+    #[arg(short, long)]
+    rbm: usize,
+    /// GMT RBM value
+    #[arg(short, long)]
+    value: f64,
+}
+#[derive(Args, Clone, Default)]
+struct Mode {
+    /// GMT M1 modal coefficient index
+    #[arg(short, long)]
+    index: usize,
+    /// GMT M1 modal coefficient value
+    #[arg(short)]
+    b: f64,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
+
+    let cli = Cli::parse();
 
     let gmtb = Gmt::builder().m1(config::m1::segment::MODES, config::m1::segment::N_MODE);
     let omb = OpticalModelBuilder::from(
@@ -56,46 +93,58 @@ async fn main() -> anyhow::Result<()> {
     // println!("{sh48_kern}");
 
     let mut m1_rbm_buf = vec![vec![0f64; 6]; 7];
-    m1_rbm_buf[0][0] = 1. * 1.1e-6; // M1S1-Tx:
-    m1_rbm_buf[1][1] = 1. * 1.2e-6; // M1S2-Ty:
-    m1_rbm_buf[2][3] = 1. * 1.4e-6; // M1S3-Rx:
-    m1_rbm_buf[3][4] = 1. * 1.5e-6; // M1S4-Ry:
-    m1_rbm_buf[4][2] = 1. * 1.6e-6; // M1S5-Tz:
-    m1_rbm_buf[5][5] = 1. * 1.3e-6; // M1S5-Rz:
-    m1_rbm_buf[6][5] = 1. * 2e-6; // M1S7-Rz:
-    // let m1_rbm = m1_rbm_buf.into_iter().flatten().collect::<Vec<f64>>();
-    let mut m2_rbm_buf = vec![0f64; 42];
-    m2_rbm_buf[M2_RBM] = 0. * 3e-4; // M2S7-Rz
-    // let m2_rbm = m2_rbm_buf;
+    let mut m2_rbm_buf = vec![vec![0f64; 6]; 7];
     let mut m1_modes_buf = vec![vec![0f64; M1_BM]; 7];
-    m1_modes_buf[0][0] = 4e-6;
-    m1_modes_buf[0][2] = 5e-6;
-    // let m1_modes = m1_modes_buf.into_iter().flatten().collect::<Vec<f64>>();
-    let m1 = MirrorState::new(m1_rbm_buf, m1_modes_buf);
-    let optical_state = OpticalState::new(m1, MirrorState::from_rbms(&m2_rbm_buf));
 
-    let e2o = Estimate2OpticsState::new();
+    let Cli {
+        mirror,
+        segment,
+        rbm,
+        mode,
+        n_sample,
+    } = cli;
+    let Rbm { rbm, value } = rbm.unwrap_or_default();
+    let Mode { index, b } = mode.unwrap_or_default();
+
+    match mirror {
+        1 => {
+            m1_rbm_buf[segment - 1][rbm] = value;
+            m1_modes_buf[segment - 1][index] = b;
+        }
+        2 => {
+            m2_rbm_buf[segment - 1][rbm] = value;
+        }
+        i => panic!("found GMT mirror id={i}, expected 1 or 2"),
+    }
+
+    let m1 = MirrorState::new(m1_rbm_buf, m1_modes_buf);
+    let optical_state = OpticalState::new(
+        m1,
+        MirrorState::from_rbms(&m2_rbm_buf.into_iter().flatten().collect::<Vec<_>>()),
+    );
+
     let add = Operator::<OpticalState>::new();
 
     let on_axis = OpticalModel::<NoSensor>::builder().gmt(gmtb).build()?;
     let print = Print::default();
     let gif = Gif::new("qp-wavefront.gif", 512, 512)?;
 
-    let n_sample = 50;
+    // let n_sample = 50;
     let timer: Timer = Timer::new(n_sample);
 
     type Sh48Frame = KernelFrame<ActiveOptics<1, M1_RBM, M2_RBM, M1_BM, N_MODE>>;
     actorscript!(
-      #[labels(sh48="GMT\nSH48x3",add="Add",e2o="=> Optical State",sh48_kern="QP AcO")]
+      #[labels(sh48="GMT\nSH48x3",add="Add",
+          sh48_kern="QP AcO")]
       1: timer[Tick] -> on_axis[WfeRms<-9>] -> print
       1: on_axis[Wavefront] -> gif
       1: optical_state[Left<OpticsState>] -> add
       1: add[OpticsState] -> on_axis
       1: add[OpticsState] -> sh48
       // 1: timer[Tick] -> sh48[Sh48Frame] -> sh48_kern[SensorData]${48*48*2*3}
-      1: sh48[Sh48Frame]! -> sh48_kern[Estimate]${84+27*7} -> e2o//
+      1: sh48[Sh48Frame]! -> sh48_kern[Right<OpticsState>]->add// -> e2o//
       1: sh48_kern[SensorData]${48*48*2*3}
-      1: e2o[Right<OpticsState>] -> add
+      // 1: e2o[Right<OpticsState>] -> add
     );
 
     // let mut log = model_logging_1.lock().await;
