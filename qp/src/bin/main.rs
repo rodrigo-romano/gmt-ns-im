378ccaa21aa2252c6b086d7c31f7642b7222f542
Rod Conan <rconan@gmto.org>
2026-02-26 17:11:09 +0000

    qp acoqp random inputs

--- qp/src/bin/main.rs
+++ qp/src/bin/main.rs
@@ -1,4 +1,4 @@
-use clap::{Args, Parser};
+use clap::{Args, Parser, Subcommand};
 use gmt_dos_actors::actorscript;
 use gmt_dos_clients::{
     gif::{Frame, Gif},
@@ -21,6 +21,7 @@ use gmt_dos_systems_agws::{
     qp::{ActiveOptics, QP},
 };
 use interface::Tick;
+use nanorand::{Rng, WyRand};
 use std::path::Path;
 
 const N_MODE: usize = 271;
@@ -32,19 +33,48 @@ type K48 = ActiveOptics<1, 41, 41, 27, 271>;
 
 #[derive(Parser)]
 struct Cli {
-    /// GMT Mirror ID (1 or 2)
-    #[arg(short, long)]
-    mirror: usize,
-    /// GMT segment ID [1,2,...,7]
-    #[arg(short, long)]
-    segment: usize,
+    // /// GMT Mirror ID (1 or 2)
+    // #[arg(short, long)]
+    // mirror: usize,
+    // /// GMT segment ID [1,2,...,7]
+    // #[arg(short, long)]
+    // segment: usize,
+    // /// Simulation sample #
+    // #[arg(short, long, default_value_t = 20)]
+    // n_sample: usize,
+    // #[command(flatten)]
+    // rbm: Option<Rbm>,
+    // #[command(flatten)]
+    // mode: Option<Mode>,
+    #[command(subcommand)]
+    setup: Setup,
     /// Simulation sample #
     #[arg(short, long, default_value_t = 20)]
     n_sample: usize,
-    #[command(flatten)]
-    rbm: Option<Rbm>,
-    #[command(flatten)]
-    mode: Option<Mode>,
+}
+
+#[derive(Subcommand, Clone)]
+enum Setup {
+    Rand {
+        #[arg(long)]
+        m1_rbms: Option<f64>,
+        #[arg(long)]
+        m2_rbms: Option<f64>,
+        #[arg(long)]
+        m1_modes: Option<f64>,
+    },
+    Specs {
+        /// GMT Mirror ID (1 or 2)
+        #[arg(short, long)]
+        mirror: usize,
+        /// GMT segment ID [1,2,...,7]
+        #[arg(short, long)]
+        segment: usize,
+        #[command(flatten)]
+        rbm: Option<Rbm>,
+        #[command(flatten)]
+        mode: Option<Mode>,
+    },
 }
 
 #[derive(Args, Clone, Default)]
@@ -98,26 +128,55 @@ async fn main() -> anyhow::Result<()> {
     let mut m2_rbm_buf = vec![vec![0f64; 6]; 7];
     let mut m1_modes_buf = vec![vec![0f64; M1_BM]; 7];
 
-    let Cli {
-        mirror,
-        segment,
-        rbm,
-        mode,
-        n_sample,
-    } = cli;
-    let Rbm { rbm, value } = rbm.unwrap_or_default();
-    let Mode { index, b } = mode.unwrap_or_default();
+    match cli.setup {
+        Setup::Rand {
+            m1_rbms,
+            m2_rbms,
+            m1_modes,
+        } => {
+            let mut rng = WyRand::new();
+            if let Some(m1) = m1_rbms {
+                println!("M1 RBMs (micro units)");
+                variates(&mut m1_rbm_buf, &mut rng, m1);
+            }
+            if let Some(m2) = m2_rbms {
+                println!("M2 RBMs (micro units)");
+                variates(&mut m2_rbm_buf, &mut rng, m2);
+            }
+            if let Some(m1) = m1_modes {
+                println!("M1 modes (micro units)");
+                variates(&mut m1_modes_buf, &mut rng, m1);
+            }
+        }
+        Setup::Specs {
+            mirror,
+            segment,
+            rbm,
+            mode,
+        } => {
+            let Rbm { rbm, value } = rbm.unwrap_or_default();
+            let Mode { index, b } = mode.unwrap_or_default();
 
-    match mirror {
-        1 => {
-            m1_rbm_buf[segment - 1][rbm] = value;
-            m1_modes_buf[segment - 1][index] = b;
+            match mirror {
+                1 => {
+                    m1_rbm_buf[segment - 1][rbm] = value;
+                    m1_modes_buf[segment - 1][index] = b;
+                }
+                2 => {
+                    m2_rbm_buf[segment - 1][rbm] = value;
+                }
+                i => panic!("found GMT mirror id={i}, expected 1 or 2"),
+            }
         }
-        2 => {
-            m2_rbm_buf[segment - 1][rbm] = value;
-        }
-        i => panic!("found GMT mirror id={i}, expected 1 or 2"),
     }
+
+    // let Cli {
+    //     mirror,
+    //     segment,
+    //     rbm,
+    //     mode,
+    //     n_sample,
+    // } = cli;
 
     let m1 = MirrorState::new(m1_rbm_buf, m1_modes_buf);
     let optical_state = OpticalState::default().zero_point(OpticalState::new(
@@ -132,7 +191,7 @@ async fn main() -> anyhow::Result<()> {
     let frame = Frame::<f32>::new("sh48-frame.png", 48 * 8);
     let sampler = Sampler::default();
 
-    let timer: Timer = Timer::new(n_sample);
+    let timer: Timer = Timer::new(cli.n_sample);
 
     type AgwsSh48 = Sh48<1>;
     type AgwsSh48Kernel = Sh48Kern<K48>;
@@ -140,12 +199,14 @@ async fn main() -> anyhow::Result<()> {
 
     actorscript!(
       #[model(name=acoqp)]
+      #[labels(sampler="1:10", on_axis="On-axis GMT",
+      frame="SH48 frame")]
       1: timer[Tick] -> optical_state[OpticsState] -> {agws::AgwsSh48}
       1: {agws::AgwsSh48Kernel}[OpticsState] -> optical_state
       1: optical_state[OpticsState] -> on_axis[WfeRms<-9>] -> print
-      1: on_axis[Wavefront] -> gif
+      1: on_axis[Wavefront].. -> gif
       1: {agws::AgwsSh48}[Sh48Frame] -> sampler
-      10: sampler[Sh48Frame] -> frame
+      10: sampler[Sh48Frame].. -> frame
     );
 
     // let mut log = model_logging_1.lock().await;
@@ -166,4 +227,17 @@ async fn main() -> anyhow::Result<()> {
     // });
 
     Ok(())
+}
+
+fn variates(buffer: &mut Vec<Vec<f64>>, rng: &mut WyRand, bound: f64) {
+    buffer.iter_mut().for_each(|seg| {
+        seg.iter_mut().for_each(|v| {
+            *v = (rng.generate::<f64>() * 2. - 1.) * bound;
+        })
+    });
+    buffer.iter().enumerate().for_each(|(i, seg)| {
+        print!(" {}: ", i + 1);
+        seg.iter().for_each(|v| print!("{:+10.3e}", v * 1e6));
+        println!("");
+    });
 }
