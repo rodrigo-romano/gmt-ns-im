978143b89dba390ee3a9ab0a8da19770f1d1068f
Rod Conan <rconan@gmto.org>
2026-03-09 18:19:59 +0000

    qp sh24 M1 shape scopes

--- qp/src/sh24.rs
+++ qp/src/sh24.rs
@@ -1,13 +1,13 @@
 mod merge_agws;
 mod merged_reconstructor;
-use gmt_dos_clients_io::gmt_m1;
+use gmt_dos_clients_io::gmt_m1::{self, segment::ModeShapes};
 use gmt_dos_clients_optics_state::MirrorState;
 use interface::{Data, UniqueIdentifier, Write};
 pub use merge_agws::MergeAgws;
 pub use merged_reconstructor::Sh48MergerReconstructor;
 
 pub const M1_N_MODE: usize = 9;
-pub const TXY_RESIDUAL_SCALING: f64 = 100.0;
+pub const TXY_RESIDUAL_SCALING: f64 = 150.0;
 
 pub enum M1RBM<const ID: u8> {}
 impl<const ID: u8> UniqueIdentifier for M1RBM<ID> {
@@ -29,6 +29,13 @@ pub enum M1RBMScope {
     Scope(M1RBM<5>),
     Scope(M1RBM<6>),
     Scope(M1RBM<7>),
+    Scope(ModeShapes<1>),
+    Scope(ModeShapes<2>),
+    Scope(ModeShapes<3>),
+    Scope(ModeShapes<4>),
+    Scope(ModeShapes<5>),
+    Scope(ModeShapes<6>),
+    Scope(ModeShapes<7>),
 }
 
 pub enum M2RBM<const ID: u8> {}
