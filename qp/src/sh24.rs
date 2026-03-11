ae5a8a60ac2cdbf297002420c63bdf27563a2477
Rod Conan <rconan@gmto.org>
2026-03-11 19:08:00 +0000

    main M2 Txy closed-loop control

--- qp/src/sh24.rs
+++ qp/src/sh24.rs
@@ -1,5 +1,6 @@
 mod merge_agws;
 mod merged_reconstructor;
+pub mod calibration;
 use gmt_dos_clients_io::gmt_m1::{self, segment::ModeShapes};
 use gmt_dos_clients_optics_state::MirrorState;
 use interface::{Data, UniqueIdentifier, Write};
