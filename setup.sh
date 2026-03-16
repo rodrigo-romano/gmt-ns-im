9716534388c4b15c7a4be70c7af8270311b0c5a5
Rod Conan <rconan@gmto.org>
2026-03-16 19:41:33 +0000

    main add M1 polishing residual errors

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ export FEM_REPO=$HOME/mnt/20230530_1756_zen_30_M1_202110_FSM_202305_Mount_202305
 # mount model 
 export MOUNT_MODEL=MOUNT_FDR_1kHz
 # flowchart layout
-export FLOWCHART=dot
+export FLOWCHART=neato
 # full path to CUDA compiler
 export CUDACXX=/usr/local/cuda/bin/nvcc
 # IP address to data server for scopes (only for AWS machine, comment out otherwise)
